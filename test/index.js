8ab2520e819c2d1e83ea8a1cb6e6d960d4b38cc6
Ben Elsen <ben.elsen@gmail.com>
2013-05-19 00:38:29 +0200

    [FIX] case (SpaceTrack)

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
+/*global describe, it */
 var should = require('should');
 
-var spacetrack = require('./../');
+var SpaceTrack = require('./../src/spacetrack');
 
-describe('spacetrack', function() {
+describe('SpaceTrack', function() {
+  'use strict';
 
   it('should exist', function() {
-    should.exist(spacetrack);
+    should.exist(SpaceTrack);
   });
 
-  it('should be a object', function() {
-    spacetrack.should.be.a('object');
+  it('should be a function', function() {
+    SpaceTrack.should.be.a('function');
   });
 
 });
