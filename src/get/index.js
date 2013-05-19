f1859024cc65fe1793a27acf6aea36fb0f5f882d
Ben Elsen <ben.elsen@gmail.com>
2013-05-19 02:13:00 +0200

    [FIX] stupid stupid…

--- src/get/index.js
+++ src/get/index.js
@@ -1,36 +1,40 @@
 (function(){
   'use strict';
 
-  var base = require('../common');
+  var common = require('../common');
 
   var util = require('util');
 
-  var get = function (options) {
+  var buildURL = function (options) {
 
     if ( !options )
-      throw new Error('#get requires ´options´ to be defined');
+      throw new Error('´options´ undefined');
 
     if ( !options.type )
-      throw new Error('#get requires ´options.type´ to be set');
+      throw new Error('´options.type´ required to be set');
 
     // Required options (for now)
     options.metadata = true;
     options.emptyresult = 'show';
     options.format = 'json';
 
+    // controller
     if ( !options.controller || ['basicspacedata', 'expandedspacedata'].indexOf(options.controller) === -1 )
       options.controller = 'basicspacedata';
 
+    // action
     if ( !options.action || ['query', 'modeldef'].indexOf(options.action) === -1 )
       options.action = 'query';
 
-    var url = base.baseURL +
+    var url = common.baseURL +
               '/' + options.controller +
               '/' + options.action +
               '/class/' + options.type;
 
+    // query
+    // must be array of objects [{field:'predicate', condition:'now-7--now'}]
     if ( options.query ) {
-      if ( !util.isOb (options.query) )
+      if ( !util.isArray(options.query) )
         options.query = [options.query];
 
       options.query.forEach(function(q) {
@@ -38,21 +42,24 @@
       });
     }
 
+    // predicates
     // must be array ['OBJECT_NAME','ORDINAL']
-    if ( options.predicates ) {
+    if ( options.predicates && options.predicates.length > 0 ) {
       if ( !util.isArray(options.predicates) )
         options.predicates = [options.predicates];
       url += '/predicates/' + options.predicates.join();
     }
 
-    if ( options.favorites ) {
+    // favorites
+    if ( options.favorites && options.favorites.length > 0 ) {
       if ( !util.isArray(options.favorites) )
         options.favorites = [options.favorites];
       url += '/favorites/' + options.favorites.join();
     }
 
+    // orderby
     // must be array ['+OBJECT_NAME','-ORDINAL']
-    if ( options.orderby ) {
+    if ( options.orderby && options.orderby.length > 0 ) {
       if ( !util.isArray(options.orderby) )
         options.orderby = [options.orderby];
 
@@ -62,6 +69,7 @@
 
     }
 
+    // limit & offset
     if ( isFinite(options.limit) || isFinite(options.offset) ) {
       if ( !isFinite(options.offset) ) options.offset = 0;
       if ( !isFinite(options.limit) ) options.limit = 100;
@@ -69,17 +77,29 @@
       url += '/limit/' + options.limit + ',' + options.offset;
     }
 
+    // distinct
     if ( options.distinct )
       url += '/distinct/true';
 
+    // metadata
     if ( options.metadata )
       url += '/metadata/true';
 
+    // emptyresult
     if ( options.emptyresult === 'show' )
       url += '/emptyresult/show';
 
+    // format
     if ( ['json', 'xml', 'html', 'csv', 'tle', '3le'].indexOf(options.format) > -1 )
       url += '/format/' + options.format;
+
+    return url;
+
+  };
+
+  var get = function (options) {
+
+    var url = buildURL(options);
 
     return url;
 
--- test/get.js
+++ test/get.js
@@ -3,18 +3,21 @@
 var get = require('./../src/get');
 
 console.log( get({
-  type: 'tle',
+  type: 'tle_latest',
+  query: [
+    {field:'NORAD_CAT_ID', condition: '25544'}
+  ],
   predicates: [
-    'OBJECT_NAME',
-    'INCLINATION',
-    'FILE'
+    // 'OBJECT_NAME',
+    // 'INCLINATION',
+    // 'FILE'
   ],
   orderby: [
     '+ORDINAL',
-    '-NORAD_CAT_ID'
+    // '-NORAD_CAT_ID'
   ],
   limit: 100,
-  offset: 20,
-  distinct: true,
-  favorites: 'Navigation'
+  // offset: 20,
+  // distinct: true,
+  // favorites: 'Navigation'
 }) );
