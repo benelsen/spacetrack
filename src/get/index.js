9874f8cf800bb861bf120c36c2e3a89e3ea852ec
Ben Elsen <ben.elsen@gmail.com>
2013-05-19 13:32:01 +0200

    [CHANGE] #get implemented as intended

--- src/get/index.js
+++ src/get/index.js
@@ -5,6 +5,8 @@
 
   var util = require('util');
 
+  var request = require('request');
+
   var buildURL = function (options) {
 
     if ( !options )
@@ -121,12 +123,41 @@
 
   };
 
-  var get = function (options) {
+  var get = function (options, callback) {
 
     var url = buildURL(options);
 
-    return url;
+    var _this = this;
 
+    request(url, {
+      jar: this.cookieJar
+    }, function(err, res, body) {
+
+      if ( err )
+        return callback(err, null);
+
+      if ( res.statusCode === 200 ) {
+        var data = JSON.parse(body);
+        return callback(null, data);
+      }
+
+      if ( res.statusCode === 401 ) {
+        _this.loggedIn = false;
+
+        return _this.login(function(err) {
+          if ( err )
+            throw err;
+          else {
+            _this.loggedIn = true;
+            _this.get( options, callback );
+          }
+
+        });
+      }
+
+      return callback(new Error('unexpected error'), null);
+
+    });
   };
 
   module.exports = get;
--- test/get.js
+++ test/get.js
@@ -1,8 +1,15 @@
 'use strict';
 
-var get = require('./../src/get');
+var config = JSON.parse(require('fs').readFileSync(__dirname+'/config.json', 'utf8'));
 
-console.log( get({
+var SpaceTrack = require('./../src/spacetrack');
+
+var st = new SpaceTrack({
+  username: config.username,
+  password: config.password
+});
+
+st.get({
   type: 'tle_latest',
   query: [
     {field:'NORAD_CAT_ID', condition: '25544'}
@@ -13,11 +20,14 @@ console.log( get({
     // 'FILE'
   ],
   orderby: [
-    '+ORDINAL',
+    '+ORDINAL'
     // '-NORAD_CAT_ID'
   ],
-  limit: 100,
+  limit: 100
   // offset: 20,
   // distinct: true,
   // favorites: 'Navigation'
-}) );
+}, function(err, data) {
+  console.error(err);
+  console.dir(data);
+});
